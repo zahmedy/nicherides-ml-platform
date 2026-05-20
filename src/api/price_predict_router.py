cfa707618e327d7f858aa835da85fceca28d5d75
Zayed Ahmed <zayed967@yahoo.com>
2026-05-20 18:23:21 -0400

    log important data

--- src/api/price_predict_router.py
+++ src/api/price_predict_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from datetime import datetime
 import pandas as pd
+
 
 from src.inference.predict_price import predict
 from src.api.schemas import CarFeatures
@@ -10,6 +12,12 @@ router = APIRouter()
 def predict_price(data: CarFeatures):
     payload = data.model_dump() if hasattr(data, "model_dump") else data.dict()
     price, model_name, model_version = predict(pd.DataFrame([payload]))
+
+    with open('../reports/price_model_report.md', 'a') as f:
+        f.write(f"Time & Date: {datetime.now()} Predicted Price: {price}  \
+                Model Name: {model_name} Model Version: {model_version}  \
+                Make: {payload['make']} Model: {payload['model']}  \
+                Year: {payload['year']} mileage: {payload['mileage']}")
 
     return {
             "model_name": model_name, 
